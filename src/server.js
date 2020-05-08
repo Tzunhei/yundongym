062c4d1151cf7b9a6ef97dd4e7e244dac4af8170
Clément Yam <yamtzunhei@gmail.com>
2020-05-08 17:50:35 +0200

    feat: add sequelize & pg npm package

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import { ApolloServer } from 'apollo-server';
 import { schema } from './modules/schema';
+import { sequelize } from './db';
 
 const server = new ApolloServer({ schema });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
