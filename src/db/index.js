e01c44edc3f1a3bb1a0358919b385367805b7db2
Clément Yam <yamtzunhei@gmail.com>
2020-05-09 10:50:28 +0200

    feat: setup local database

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,13 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize();
+const {
+  DATABASE_USERNAME: username,
+  DATABASE_PASSWORD: password,
+} = process.env;
+
+const sequelize = new Sequelize('yundongym', username, password, {
+  host: 'localhost',
+  dialect: 'postgres',
+});
 
 export { sequelize };
