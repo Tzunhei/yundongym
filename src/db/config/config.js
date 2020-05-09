394542466e3837b8940e7d1fd1d8e3eadf33fd2b
Clément Yam <yamtzunhei@gmail.com>
2020-05-09 14:51:56 +0200

    init sequelize db
    created scripts for sequelize db operations
    
    init auth module

---
+++ src/db/config/config.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: 'yundongym',
+    host: 'localhost',
+    dialect: 'postgres',
+    operatorsAliases: false,
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    operatorsAliases: false,
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    operatorsAliases: false,
+  },
+};
