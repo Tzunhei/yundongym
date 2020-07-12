c5b50aba275c063501272bf462a82e5f18cc849d
Clément Yam <yamtzunhei@gmail.com>
2020-07-12 12:15:20 +0200

    feat(exercise): init get & create resolvers

--- src/app/modules/exercise/resolvers.js
+++ src/app/modules/exercise/resolvers.js
@@ -1,6 +1,24 @@
 const resolvers = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    getExercises: async (_, { id }, { models }) => {
+      const { Exercise } = models;
+      if (id) return await Exercise.findOne({ where: { id } });
+
+      return await Exercise.findAll({});
+    },
+  },
+  Mutation: {
+    createExercise: async (_, { input }, { models }) => {
+      const { Exercise } = models;
+      const { name, muscleGroup } = input;
+      const exercise = await Exercise.create({
+        name,
+        muscleGroup,
+      });
+
+      return exercise;
+    },
+  },
 };
 
 export default resolvers;
